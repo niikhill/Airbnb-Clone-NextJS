85e8f73468a1d2de549bed27a2ca41de3c991bd3
niikhill <5293647+niikhill@users.noreply.github.com>
2021-08-05 18:57:52 +0530

    Explore Cards

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
-export default function Home() {
+import ExploreCard from "../components/ExploreCard";
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -13,9 +14,32 @@ export default function Home() {
       <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item) => (
+              <ExploreCard
+                key={item.location}
+                img={item.img}
+                distance={item.distance}
+                location={item.location}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
         </section>
       </main>
     </div>
   );
 }
 
+export async function getStaticProps() {
+  const exploreData = await fetch("https://jsonkeeper.com/b/UMTE").then((res) =>
+    res.json()
+  );
+  return {
+    props: {
+      exploreData,
+    },
+  };
+}
